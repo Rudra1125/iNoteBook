27c509b33943607b3d2f3ebaabb2376d8942c112
Rudra1125 <shankarrudra36@gmail.com>
2022-01-30 21:15:48 +0530

    Update note end point created

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -55,4 +55,38 @@ router.post(
   }
 );
 
+// Route 3: 
+// Update an exsisting Note
+// Post "/api/Notes/updatenote" Login required 
+
+router.put('/updatenote/:id',fetchuser, async (req,res)=>{
+  const {title,description,tag}=req.body;
+  // Create a newNote Object
+  const newNote = {};
+  if(title){
+    newNote.title = title;
+  }
+  if(description){
+    newNote.description = description ;
+  }
+  if(tag){
+    newNote.tag = tag;
+  }
+  // here new note object has been built
+  // find the note to be updated or update it
+  
+  let note = await Notes.findById(req.params.id);
+  
+  // if note is present or not then we use this check
+  if(!note){
+    return res.status(404).send("Not Found");
+  }
+  //  if some one else want to access then we use this check
+  if(note.user.toString()!==req.user.id){
+    return res.status(401).send("Not allowed");
+  }
+  note = await Notes.findByIdAndUpdate(req.params.id, {$set:newNote}, {new:true})
+  res.json({note});
+
+});
 module.exports = router;
